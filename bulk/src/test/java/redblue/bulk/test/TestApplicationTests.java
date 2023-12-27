7e531b7d9aa6e706154d741de4859892e8d45d3f
jihoonLeee <jihoon2723@naver.com>
2023-12-27 23:57:22 +0900

    init

---
+++ bulk/src/test/java/redblue/bulk/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package redblue.bulk.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
